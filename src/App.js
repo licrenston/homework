b62cfb089578534cb4a12b213d28075799b3df0b
licrenston <bserhiy@gmail.com>
2022-10-16 13:05:12 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import {getUsers} from "./services/user.service";
+import UserComponent from "./components/UserComponent";
 
 
 function App() {
 
+    let [users, setUsers] = useState([]);
+    let [chosenUser, setChosenUser] = useState(null);
+
+    useEffect(() => {
+        getUsers().then(value => setUsers(value.data));
+    }, [])
+
+    const chooseUser = (id) => {
+        getUsers(id).then(value => setChosenUser(value.data))
+    }
+
     return (
         <div>
-
+            {
+                chosenUser && (<div>{chosenUser.name}</div>)
+            }
+            <hr/>
+            {users.map(value =>
+                <UserComponent
+                    key={value.id}
+                    item={value}
+                    chooseUser={chooseUser}
+                />)}
         </div>
     );
 }
