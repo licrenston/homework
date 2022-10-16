7bddd9e9218dc32f30ac18e50fda95f1b8e0edab
licrenston <bserhiy@gmail.com>
2022-10-16 11:15:49 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import UserComponent from "./UserComponent";
+
 
 function App() {
-    let [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(value => {
-                setUsers(value);
-                console.log(value);
-            });
-    }, []);
 
     return (
         <div>
-            {
-                users.map(value => <UserComponent
-                    key={value.id} item={value}/>)
-            }
+
         </div>
     );
 }
